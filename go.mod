dbb990a8459ffbb0e447d6e4c033958ac5395b65
zhangyangle <zhangyangle@bytedance.com>
2021-03-22 17:06:43 +0800

    update:remove gmail

--- go.mod
+++ go.mod
@@ -7,12 +7,13 @@ require (
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.3.5 // indirect
 	github.com/bitly/go-simplejson v0.5.0
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
--- main.go
+++ main.go
@@ -4,19 +4,12 @@ import (
     "fmt"
     "github.com/bitly/go-simplejson"
     "github.com/gocolly/colly"
-    "gopkg.in/gomail.v2"
-    "os"
     "time"
 )
 
-var(
-    gmailPwd string
-)
+const JisiluUrl = "https://www.jisilu.cn/data/lof/stock_lof_list/?___jsl=LST___t=%d&rp=25&page=1"
 
 func main() {
-    gmailPwd = os.Getenv("gmail_pwd")
-    fmt.Println("gmail_pwd", gmailPwd)
-
     // Instantiate default collector
     c := colly.NewCollector()
     c.OnResponse(func(response *colly.Response) {
@@ -35,25 +28,11 @@ func main() {
             }
         }
     })
-    url := fmt.Sprintf("https://www.jisilu.cn/data/lof/stock_lof_list/?___jsl=LST___t=%d&rp=25&page=1", time.Now().UnixNano())
+    url := fmt.Sprintf(JisiluUrl, time.Now().UnixNano())
     err := c.Visit(url)
     if err != nil {
         fmt.Println(err)
     }
 
-    sendEmail()
 }
 
-func sendEmail() {
-    m := gomail.NewMessage()
-    m.SetHeader("From", "znyalor@gmail.com")
-    m.SetHeader("To", "657345933@qq.com")
-    m.SetHeader("Subject", "Hello!")
-    m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
-
-    d := gomail.NewDialer("smtp.gmail.com", 465, "znyalor@gmail.com", gmailPwd)
-    if err := d.DialAndSend(m); err != nil {
-        fmt.Println("sendEmail err", err)
-        panic(err)
-    }
-}
